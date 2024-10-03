aa81dbc9b12aba9ff1f1a9ce96e926f5265cbc8d
vijay <vijayreddy6812@gmail.com>
2024-10-03 19:44:46 -0400

    mut

--- project0/main.py
+++ project0/main.py
@@ -34,7 +34,7 @@ def extractincidents(pdf_file_path):
     # Regex patterns to capture each field
     date_time_pattern = r'(\d{1,2}/\d{1,2}/\d{4} \d{1,2}:\d{2})'
     incident_number_pattern = r'(\d{4}-\d{5,8})'
-    location_pattern = r"([A-Z0-9][\w\s./,;'()-]*?(?:RAMP\s\d+\sRAMP)?(?=\s(?:911|MVA|COP|EMS|[A-Z][a-z/])))"
+    location_pattern = r"([A-Z0-9][\w\s./,;'()-]*?(?:RAMP\s\d+\sRAMP)?(?=\s(?:911|MVA|COP|EMS Mutual Aid|[A-Z][a-z/])))"
     nature_pattern = r'(911(?:\s+[A-Z][a-zA-Z\s]+(?:/[A-Za-z\s]+)*)?|[A-Z][a-zA-Z\s]+(?:/[A-Za-z\s]+)*)'
     ori_pattern = r'(OK\d+|EMSSTAT|\d{5})'
 
@@ -63,9 +63,7 @@ def extractincidents(pdf_file_path):
                 if "Vandalism" in line:
                     incident["nature"] = "Vandalism"  # Force the nature to be 'Larceny'
                 incidents.append(incident)
-                if "EMS Mutual Aid" in line:
-                    incident["nature"] = "EMS Mutual Aid"  # Force the nature to be 'Larceny'
-                incidents.append(incident)
+                
         
         return incidents
     except Exception as e:
